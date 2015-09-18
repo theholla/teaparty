e4272010ea2b571d2e24649bd34688f41570283d
the holla <holla@bikefarm.org>
2015-09-18 16:32:49 -0700

    add behavior for index 0, 1, and 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,18 @@
 function printSong(cups) {
   var couplets = [];
-  for (i = cups; i > 0; i--) {
-    couplets.push("He gave " + i + " cups of tea to Alice, " + i + " cups of tea! \n" + "He took one down, and passed it around, " + (i - 1) + " cups of tea for her! \n");
+  var i = "";
+  debugger;
+
+  for (i = cups; i >= 0; i--) {
+    if (i === 2) {
+      couplets.push("He gave " + i + " cup of tea to Alice, " + i + " cup of tea! \n" + "He took it down, and passed it around, " + (i - 1) + " cup of tea for her! \n");
+    } else if (i === 1) {
+      couplets.push("He gave " + i + " cup of tea to Alice, " + i + " cup of tea! \n" + "He took it down, and passed it around, " + (i - 1) + " cups of tea for her! \n");
+    } else if (i === 0) {
+      couplets.push("He gave no more cups of tea to Alice, no cups of tea! \n" + "He laid on the floor, imagined some more... " + cups + " cups of tea for her! \n");
+    } else {
+      couplets.push("He gave " + i + " cups of tea to Alice, " + i + " cups of tea! \n" + "He took one down, and passed it around, " + (i - 1) + " cups of tea for her! \n");
+    }
   }
   return couplets.join("");
 }
