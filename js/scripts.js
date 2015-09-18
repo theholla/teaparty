6259caf3bec7bb313e5cca3650757be4c7f8fdcc
the holla <holla@bikefarm.org>
2015-09-18 14:01:53 -0700

    printSong decrements through song correctly

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,22 @@
-
+function printSong(cups) {
+  for (i = cups; i > 0; i--) {
+    console.log(i);
+      document.write("He gave " + i + " cups of tea to Alice, ");
+      document.write(i + " cups of tea! ");
+      document.write("He took one down, and passed it around, ");
+      document.write((i -= 1) + " cups of tea for her! ");
+  }
+}
 
 $(document).ready(function() {
   $("form#teacups").submit(function(event) {
     var cups = parseInt($("input#cups").val());
-    var output = getPoem(cups);
+    var output = printSong(cups);
 
     $(".output").text(output);
 
     $("#result").show();
+    $("#finalThought").show();
     event.preventDefault();
   });
 });
