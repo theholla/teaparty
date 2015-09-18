136ec546e5a21f4e2c9bae685b890a9668edc79a
the holla <holla@bikefarm.org>
2015-09-18 13:05:40 -0700

    Add specs

---
+++ spec/specs.js
@@ -0,0 +1,13 @@
+describe("printSong", function() {
+  it("displays couplets for all numbers between a number (user's choice) and 0", function() {
+    expect(printSong(4)).to.have.string("4 cups of tea");
+  })
+
+  it("uses standard grammar when there is only one cup", function() {
+    expect(printSong(4)).to.have.string("1 cup of tea");
+  })
+
+  it("uses standard grammar when there are no cups of tea", function() {
+    expect(printSong(5)).to.have.string("no cups of tea");
+  })
+})
