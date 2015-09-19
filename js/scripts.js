9f5fe7d55da227e2beb0eef7e1e546d8c7a5e07e
the holla <holla@bikefarm.org>
2015-09-18 17:46:26 -0700

    Tests pass

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
 function printSong(cups) {
   var couplets = [];
   var i = "";
-  debugger;
 
   for (i = cups; i >= 0; i--) {
     if (i === 2) {
@@ -9,7 +8,7 @@ function printSong(cups) {
     } else if (i === 1) {
       couplets.push("He gave " + i + " cup of tea to Alice, " + i + " cup of tea! \n" + "He took it down, and passed it around, " + (i - 1) + " cups of tea for her! \n");
     } else if (i === 0) {
-      couplets.push("He gave no more cups of tea to Alice, no cups of tea! \n" + "He laid on the floor, imagined some more... " + cups + " cups of tea for her! \n");
+      couplets.push("He gave no more cups of tea to Alice, no more cups of tea! \n" + "He laid on the floor, imagined some more... " + cups + " cups of tea for her! \n");
     } else {
       couplets.push("He gave " + i + " cups of tea to Alice, " + i + " cups of tea! \n" + "He took one down, and passed it around, " + (i - 1) + " cups of tea for her! \n");
     }
@@ -21,7 +20,6 @@ $(document).ready(function() {
   $("form#teacups").submit(function(event) {
     var cups = parseInt($("input#cups").val());
     var output = printSong(cups);
-
     $(".output").text(output);
 
     $("#result").show();
--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,6 @@
 describe("printSong", function() {
   it("displays couplets for all numbers between a number (user's choice) and 0", function() {
-    expect(printSong(4)).to.have.string("4 cups of tea");
+    expect(printSong(4)).to.have.string("3 cups of tea");
   })
 
   it("uses standard grammar when there is only one cup", function() {
@@ -8,6 +8,6 @@ describe("printSong", function() {
   })
 
   it("uses standard grammar when there are no cups of tea", function() {
-    expect(printSong(5)).to.have.string("no cups of tea");
+    expect(printSong(5)).to.have.string("no more cups of tea");
   })
 });
