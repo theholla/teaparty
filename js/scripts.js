358378cfdf983814a02bbe0d962cf46324a2b339
the holla <holla@bikefarm.org>
2015-09-23 07:50:13 -0700

    Fix line presentation

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,13 @@ function printSong(cups) {
 
   for (i = cups; i >= 0; i--) {
     if (i === 2) {
-      couplets.push("He gave " + i + " cup of tea to Alice, " + i + " cup of tea! \n" + "He took it down, and passed it around, " + (i - 1) + " cup of tea for her! \n");
+      couplets.push("<p>He gave " + i + " cups of tea to Alice, " + i + " cups of tea! <br>" + "He took one down, and passed it around, " + (i - 1) + " more cup of tea for her! </p>");
     } else if (i === 1) {
-      couplets.push("He gave " + i + " cup of tea to Alice, " + i + " cup of tea! \n" + "He took it down, and passed it around, " + (i - 1) + " cups of tea for her! \n");
+      couplets.push("<p>He gave " + i + " cup of tea to Alice, " + i + " cup of tea! <br>" + "He took it down, and passed it around, no more cups of tea for her! </p>");
     } else if (i === 0) {
-      couplets.push("He gave no more cups of tea to Alice, no more cups of tea! \n" + "He laid on the floor, imagined some more... " + cups + " cups of tea for her! \n");
+      couplets.push("<p>He gave no more cups of tea to Alice, no more cups of tea! <br>" + "He laid on the floor, imagined some more... " + cups + " cups of tea for her! </p>");
     } else {
-      couplets.push("He gave " + i + " cups of tea to Alice, " + i + " cups of tea! \n" + "He took one down, and passed it around, " + (i - 1) + " cups of tea for her! \n");
+      couplets.push("<p>He gave " + i + " cups of tea to Alice, " + i + " cups of tea! <br>" + "He took one down, and passed it around, " + (i - 1) + " cups of tea for her! </p>");
     }
   }
   return couplets.join("");
@@ -20,7 +20,7 @@ $(document).ready(function() {
   $("form#teacups").submit(function(event) {
     var cups = parseInt($("input#cups").val());
     var output = printSong(cups);
-    $(".output").text(output);
+    $(".output").html(output);
 
     $("#result").show();
     $("#finalThought").show();
